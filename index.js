0ec40284c1e0f8758e25d8b9cfbb148ce9b4ec03
Brandon-Vu <brandonvu20052005@gmail.com>
2025-11-13 22:21:05 +0000

    Task B

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+var mysql = require('mysql2');
+
+const db = mysql.createPool({
+  host: 'localhost',
+  user: 'berties_books_app',
+  password: 'qwertyuiop',
+  database: 'berties_books',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+global.db = db;
+
 // Import express and ejs
 var express = require ('express')
 var ejs = require('ejs')
