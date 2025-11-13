bd5577bb080b3c59a60cc3b727b2dc782f4a5dbc
Brandon-Vu <brandonvu20052005@gmail.com>
2025-11-13 23:23:24 +0000

    Final Lab 6 updates + links.txt

--- routes/books.js
+++ routes/books.js
@@ -6,9 +6,23 @@ router.get('/search',function(req, res, next){
     res.render("search.ejs")
 });
 
-router.get('/search-result', function (req, res, next) {
-    //searching in the database
-    res.send("You searched for: " + req.query.keyword)
+router.get('/search_result', function (req, res, next) {
+  const keyword = req.query.search_text;
+
+  // Perform the database query to find books matching the keyword
+  let sqlquery = "SELECT * FROM books WHERE name LIKE ?";
+  let searchTerm = '%' + keyword + '%';
+
+  db.query(sqlquery, [searchTerm], (err, result) => {
+    if (err) {
+      next(err);
+    } else {
+      res.render('searchresult.ejs', {
+        keyword: keyword,
+        results: result
+      });
+    }
+  });
 });
 
 // Export the router object so index.js can access it
@@ -21,7 +35,45 @@ router.get('/list', function (req, res, next) {
     if (err) {
       next(err);
     } else {
-      res.send(result); 
+      res.render('list.ejs', { availableBooks: result });
     }
   });
 });
+
+// Show the add book form
+router.get('/addbook', function (req, res) {
+  res.render('addbook.ejs');
+});
+
+// Handle the add book form submission
+router.post('/bookadded', function (req, res, next) {
+  let sqlquery = "INSERT INTO books (name, price) VALUES (?, ?)";
+  let newrecord = [req.body.name, req.body.price];
+
+  db.query(sqlquery, newrecord, (err, result) => {
+    if (err) {
+      next(err);
+    } else {
+      res.send(
+        'Book added: ' +
+        req.body.name +
+        ' (£' +
+        req.body.price +
+        ')'
+      );
+    }
+  });
+});
+
+// List books that are bargains (price < 20)
+router.get('/bargains', function(req, res, next) {
+  let sqlquery = "SELECT * FROM books WHERE price < 20";
+
+  db.query(sqlquery, (err, result) => {
+    if (err) next(err);
+    else res.render('list.ejs', { availableBooks: result });
+  });
+});
+
+// Export the router object so index.js can access it
+module.exports = router;
